ce73e02af87788fd408f46be740956b6b1cb0d20
altandemircan <altandemircan@gmail.com>
2025-10-08 10:14:41 +0300

    Update local_storage.js

--- public/js/local_storage.js
+++ public/js/local_storage.js
@@ -483,14 +483,14 @@ function renderMyTripsPanel() {
     const itemCount = trip.cart ? trip.cart.length : 0;
 
     const thumbInfo = document.createElement("div");
-    thumbInfo.className = "mytrips-thumb-info";
-    thumbInfo.textContent = `${dayCount} day - ${itemCount} item`;
-    thumbInfo.style.cssText = `
-      display:none;position:absolute;top:0;left:0;width:100%;height:100%;
-      background:rgba(25,28,44,0.68);color:#fff;font-size:13px;
-      display:flex;flex-direction:column;align-items:center;justify-content:center;
-      border-radius:7px;z-index:2;text-shadow:0 1px 3px #111;
-    `;
+thumbInfo.className = "mytrips-thumb-info";
+thumbInfo.innerHTML = `<div>${dayCount} day</div><div>${itemCount} item</div>`;
+thumbInfo.style.cssText = `
+  display:none;position:absolute;top:0;left:0;width:100%;height:100%;
+  background:rgba(25,28,44,0.68);color:#fff;font-size:13px;
+  display:flex;flex-direction:column;align-items:center;justify-content:center;
+  border-radius:7px;z-index:2;text-shadow:0 1px 3px #111;
+`;
 
     thumbBox.onmouseenter = () => { thumbInfo.style.display = "flex"; };
     thumbBox.onmouseleave = () => { thumbInfo.style.display = "none"; };
