e195b003e1cb1cd90f33670a83fb86a17fa39636
altandemircan <altandemircan@gmail.com>
2025-10-11 22:59:28 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -6781,15 +6781,14 @@ if (imported) {
 } else {
   // Eski haliyle devam et
   renderRouteScaleBar(
-    expandedScaleBar,
-    dist/1000,
-    samples.map((p,i)=>({
-      name: '',
-      distance: dists[i]/1000,
-      snapped: true
-    }))
-  );
-}
+  expandedScaleBar,
+  dist/1000,
+  samples.map((p, i) => ({
+    name: (i === 0 ? "Start" : (i === samples.length - 1 ? "Finish" : "")),
+    distance: dists[i]/1000,
+    snapped: true
+  }))
+);
   }
 }
 
