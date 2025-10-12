64a4d9eb4c2af84e5292d70eb2ee6659685a216c
altandemircan <altandemircan@gmail.com>
2025-10-12 16:57:47 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -8692,7 +8692,7 @@ const totalM = cum[cum.length - 1] || 1;
 
 // Örnekleme (tam profil)
 
-const N = Math.max(60, Math.round(totalKm *5));
+const N = Math.max(60, Math.round(totalKm *2));
 
 const samples = [];
 for (let i = 0; i < N; i++) {
