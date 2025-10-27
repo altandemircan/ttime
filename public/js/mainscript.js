4319ad5555aa26e0dd679cc7ccf06961393b85d7
altandemircan <altandemircan@gmail.com>
2025-10-27 16:35:49 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -2564,6 +2564,7 @@ function displayPlacesInChat(places, category, day) {
   pagination: false,
                     drag: true,
                       breakpoints: {
+                        1000: { perPage: 1 },
                         1350: { perPage: 2 },
                         1650: { perPage: 3 },
                         2000: { perPage: 4 }
