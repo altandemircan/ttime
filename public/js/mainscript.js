8d500189e70cd0b35ab4bfdba6963c7d7dcab08a
altandemircan <altandemircan@gmail.com>
2025-10-27 10:50:27 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -8297,7 +8297,7 @@ function wrapRouteControls(day) {
   expandBtn.style.padding = '4px 6px';
   expandBtn.style.fontWeight = 'bold';
   expandBtn.style.color = '#ffffff;';
-  expandBtn.style.cursor = 'pointer;'
+  expandBtn.style.cursor = 'pointer;';
 
   expandBtn.innerHTML = `
     <img class="tm-icon" src="img/see_route.gif" alt="MAP" loading="lazy" decoding="async">
