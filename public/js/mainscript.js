9f22e130803d8156572efe519809d08335431b05
altandemircan <altandemircan@gmail.com>
2025-11-05 22:08:41 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -5145,45 +5145,27 @@ function updateRouteStatsUI(day) {
 
  
 
- function openMapLibre3D(expandedMap) {
-  // Eski vektör layerı kaldır
-  if (expandedMap._maplibreLayer) {
-    expandedMap.removeLayer(expandedMap._maplibreLayer);
-    expandedMap._maplibreLayer = null;
-  }
-  // MapLibreGL JS ile 3D harita ekle
-  // Not: expandedMap Leaflet haritası! 3D için doğrudan MapLibreGL ile yeni bir div göstermek daha iyi
-  // Kısayol: Expanded harita içine yeni bir div ekle ve oraya MapLibreGL JS ile harita aç!
+function openMapLibre3D(expandedMap) {
+  // Eski maplibre 3d div varsa sil/gizle
   let mapDiv = expandedMap.getContainer();
-  // Altına bir 3D harita div'i oluştur
   let maplibre3d = document.getElementById('maplibre-3d-view');
   if (!maplibre3d) {
     maplibre3d = document.createElement('div');
     maplibre3d.id = 'maplibre-3d-view';
     maplibre3d.style.cssText = 'width:100%;height:480px;position:absolute;left:0;top:0;z-index:10000;';
-    mapDiv.appendChild(maplibre3d);
-  } else {
-    maplibre3d.innerHTML = '';
-    maplibre3d.style.display = 'block';
+    mapDiv.parentNode.appendChild(maplibre3d);
   }
-  // 3D harita aç (kitaplık import ediyor mu? check!)
-  if (!window._maplibre3DInstance) {
-    window._maplibre3DInstance = new maplibregl.Map({
-      container: 'maplibre-3d-view',
-      style: 'https://tiles.openfreemap.org/styles/liberty', // veya kendi 3d stillin
-      center: expandedMap.getCenter(), // Leaflet center'ı aynen kullan
-      zoom: expandedMap.getZoom(),
-      pitch: 60,     // 3D eğim
-      bearing: 30    // Yön açı
-    });
-    // Mevcut rotayı geojson layer olarak eklemek istiyorsan (örnek):
-    // window._maplibre3DInstance.on('load', () => {
-    //   window._maplibre3DInstance.addSource('route', {...});
-    //   window._maplibre3DInstance.addLayer({...});
-    // });
-  }
-  // Diğer harita divleri ister gizle
-  mapDiv.querySelectorAll('.leaflet-container').forEach(el => el.style.display = 'none');
+  maplibre3d.style.display = 'block';
+  // Temizle ve yeniden başlat
+  maplibre3d.innerHTML = '';
+  window._maplibre3DInstance = new maplibregl.Map({
+    container: 'maplibre-3d-view',
+    style: 'https://tiles.openfreemap.org/styles/liberty',
+    center: expandedMap.getCenter(),
+    zoom: expandedMap.getZoom(),
+    pitch: 60,
+    bearing: 30
+  });
 }
 
 async function expandMap(containerId, day) {
@@ -5262,12 +5244,20 @@ let currentLayer = 'liberty';
     div.setAttribute('data-value', opt.value);
     div.innerHTML = `<img src="${opt.img}" alt="${opt.label}"><span>${opt.label}</span>`;
     if (opt.value === currentLayer) div.classList.add('selected');
-    div.onclick = function() {
+
+    
+ div.onclick = function() {
   layersBar.querySelectorAll('.map-type-option').forEach(o => o.classList.remove('selected'));
   div.classList.add('selected');
   if (opt.value === '3d') {
+    // 3D haritayı açınca leaflet expanded'ı gizle
+    expandedMap.getContainer().style.display = "none";
     openMapLibre3D(expandedMap);
   } else {
+    // 2D (leaflet harita) görünür, 3D gizli
+    expandedMap.getContainer().style.display = "";
+    let map3d = document.getElementById('maplibre-3d-view');
+    if (map3d) map3d.style.display = "none";
     setExpandedMapTile(opt.value);
   }
 };
