e22585d182850e3dbdad5bd69f705489a51c9ea6
altandemircan <altandemircan@gmail.com>
2025-11-27 01:46:32 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -320,7 +320,7 @@ const wrapper = document.createElement('div');
 wrapper.style.cssText = `
   position: absolute;
   right: 0;
-  top: ${correctedY - 6}px;
+  top: ${correctedY - 7}px;
   display: flex;
   flex-direction: column;   /* Dikey */
   align-items: flex-start;
