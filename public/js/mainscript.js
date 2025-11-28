395dd655c09c1e70ed7fad9d7270841e8cb283e5
altandemircan <altandemircan@gmail.com>
2025-11-28 20:39:09 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -312,53 +312,53 @@ function createScaleElements(track, widthPx, spanKm, startKmDom, markers = []) {
 
   const svgH = svg ? (Number(svg.getAttribute('height')) || 180) : 180;
 
-gridLabels.forEach(obj => {
-  const trackHeight = track.clientHeight || 180;
-  const svgHeight = svg ? Number(svg.getAttribute('height')) || 180 : 180;
-  const correctedY = (obj.y / svgHeight) * trackHeight; // 👈 ORANTALA!
-const wrapper = document.createElement('div');
-wrapper.style.cssText = `
-  position: absolute;
-  right: 0;
-  top: ${correctedY - 7.5}px;
-  display: flex;
-  flex-direction: column;   /* Dikey */
-  align-items: flex-start;
-  pointer-events: none;
-  text-align: right;
-  gap: 4px;
-`;
+// gridLabels.forEach(obj => {
+//   const trackHeight = track.clientHeight || 180;
+//   const svgHeight = svg ? Number(svg.getAttribute('height')) || 180 : 180;
+//   const correctedY = (obj.y / svgHeight) * trackHeight; // 👈 ORANTALA!
+// const wrapper = document.createElement('div');
+// wrapper.style.cssText = `
+//   position: absolute;
+//   right: 0;
+//   top: ${correctedY - 7.5}px;
+//   display: flex;
+//   flex-direction: column;   /* Dikey */
+//   align-items: flex-start;
+//   pointer-events: none;
+//   text-align: right;
+//   gap: 4px;
+// `;
 
-const label = document.createElement('div');
-label.className = 'elevation-label';
-label.style.cssText = `
-  font-size: 10px;
-  color: #607d8b;
-  background: none;
-  line-height: 0.50;
-    text-align: right;
-    padding-right: 0px;
-    white-space: nowrap;
-    margin-bottom: -6px;
-`;
+// const label = document.createElement('div');
+// label.className = 'elevation-label';
+// label.style.cssText = `
+//   font-size: 10px;
+//   color: #607d8b;
+//   background: none;
+//   line-height: 0.50;
+//     text-align: right;
+//     padding-right: 0px;
+//     white-space: nowrap;
+//     margin-bottom: -6px;
+// `;
 
-label.textContent = obj.value;
+// label.textContent = obj.value;
 
-const tick = document.createElement('div');
-tick.style.cssText = `
-      width: 26px;
-    height: 8px;
-  border-bottom: 1px dashed #cfd8dc;
-  opacity: 0.7;
-  display: block;
-  margin-left: 0px;
-  margin-top: 0px;
-`;
+// const tick = document.createElement('div');
+// tick.style.cssText = `
+//       width: 26px;
+//     height: 8px;
+//   border-bottom: 1px dashed #cfd8dc;
+//   opacity: 0.7;
+//   display: block;
+//   margin-left: 0px;
+//   margin-top: 0px;
+// `;
 
-wrapper.appendChild(label);
-wrapper.appendChild(tick);
-elevationLabels.appendChild(wrapper);
-});
+// wrapper.appendChild(label);
+// wrapper.appendChild(tick);
+// elevationLabels.appendChild(wrapper);
+// });
 
   track.style.position = 'relative';
   track.appendChild(elevationLabels);
