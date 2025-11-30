c94b6148f86cbee3f7b6154098fe6b319b51e878
altandemircan <altandemircan@gmail.com>
2025-12-01 02:25:53 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -3650,9 +3650,8 @@ const points = dayItemsArr.map(it => it.location ? it.location : null).filter(Bo
           <div class="cart-item">
              <div style="display: flex; align-items: center; justify-content: space-between; width: 100%">
               <div style="display: flex; align-items: center; gap: 10px;">
-                <div class="item-position">${listMarkerHtml} 
-                  <img src="${item.image || 'img/added-note.png'}" alt="${item.name}" class="cart-image">
-                </div>
+                ${listMarkerHtml} 
+                <img src="${item.image || 'img/added-note.png'}" alt="${item.name}" class="cart-image">
                 <div class="item-info">
                   <p class="toggle-title">${item.name}</p>
                 </div>
@@ -3697,9 +3696,9 @@ const points = dayItemsArr.map(it => it.location ? it.location : null).filter(Bo
             <div style="display: flex; align-items: center; justify-content: space-between; width: 100%">
               <div style="display: flex; align-items: center; gap: 10px;">
                 
-                ${listMarkerHtml}
-                
-                <img src="https://www.svgrepo.com/show/458813/move-1.svg" alt="Drag" class="drag-icon">
+                <div class="item-position">${listMarkerHtml}                
+                  <img src="https://www.svgrepo.com/show/458813/move-1.svg" alt="Drag" class="drag-icon">
+                </div>
                 <img src="${item.image}" alt="${item.name}" class="cart-image">
                 <img src="${categoryIcons[item.category] || 'https://www.svgrepo.com/show/522166/location.svg'}" alt="${item.category}" class="category-icon">
                 <div class="item-info">
