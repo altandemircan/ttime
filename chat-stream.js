87216c6669e59d3b128eaf75d0dad6528ce4de60
altandemircan <altandemircan@gmail.com>
2026-01-11 09:38:03 +0300

    .

--- chat-stream.js
+++ chat-stream.js
@@ -2,7 +2,7 @@ const express = require('express');
 const axios = require('axios');
 const router = express.Router();
 
-router.get('/chat-stream', async (req, res) => {
+router.get('/', async (req, res) => {
     res.setHeader('Content-Type', 'text/event-stream');
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Cache-Control', 'no-cache');
