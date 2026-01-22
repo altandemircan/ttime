c2d523deade5b5202bdbec33a7aeab84760a42ac
altandemircan <altandemircan@gmail.com>
2026-01-23 01:10:04 +0300

    Update mainscript.js

--- public/js/mainscript.js
+++ public/js/mainscript.js
@@ -167,13 +167,15 @@ function showSuggestions() {
     suggestionsDiv.innerHTML = "";
 
     const options = [
-    { text: "3 days in Antalya", flag: countryFlag("TR") },
-    { text: "Explore Rome for 1 day", flag: countryFlag("IT") },
+    { text: "2 days in Antalya", flag: countryFlag("TR") },
+    { text: "Explore Rome for 3 day", flag: countryFlag("IT") },
+    { text: "1 days in Tokyo", flag: countryFlag("JP") },
     { text: "London 2-day guide", flag: countryFlag("GB") },
-    { text: "Explore Rome for 1 day", flag: countryFlag("IT") },
     { text: "3-day Paris itinerary", flag: countryFlag("FR") },
-    { text: "Visit Madrid in 2 days", flag: countryFlag("ES") }        
-    ];
+    { text: "Visit Madrid in 1 days", flag: countryFlag("ES") },
+    { text: "3 days in Bangkok", flag: countryFlag("TH") },    
+    { text: "Discover Petra for 2 day", flag: countryFlag("JO") }
+];
 
     options.forEach(option => {
         const suggestion = document.createElement("div");
