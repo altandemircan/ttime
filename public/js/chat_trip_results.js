14649fd2570bfb28d7bd5d062df0a48af004db64
altandemircan <altandemircan@gmail.com>
2026-01-27 13:46:08 +0300

    Update chat_trip_results.js

--- public/js/chat_trip_results.js
+++ public/js/chat_trip_results.js
@@ -127,7 +127,7 @@ function generateStepHtml(step, day, category, idx = 0) {
                 </select>
             </div>
             
-            <a class="addtotrip"><span>Add to trip</span>
+            <a class="addtotrip"><span>Add</span>
                 <img src="img/addtotrip-icon.svg">
             </a>
         </div>
