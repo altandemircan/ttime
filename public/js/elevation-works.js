df6a2480453aed5fb682767034702523b732dae6
altandemircan <altandemircan@gmail.com>
2026-02-09 04:32:16 +0300

    Update elevation-works.js

--- public/js/elevation-works.js
+++ public/js/elevation-works.js
@@ -2156,7 +2156,7 @@ window.showScaleBarLoading = function(c, t='Loading elevation…', day=null, sKm
       placeholder.className = 'elevation-placeholder';
       
       placeholder.style.cssText = `
-          width: 100%; height: 120px; 
+          width: 100%; height: 130px; 
           display: flex; flex-direction: column; align-items: center; justify-content: center;
           color: #6c757d; font-size: 14px;
           position: absolute; top: 0; left: 0;
